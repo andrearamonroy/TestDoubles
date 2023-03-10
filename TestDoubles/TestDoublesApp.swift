faaeeaff67f083d4b58761f41f2d8f9e8816aa82
Andrea Monroy <andrearamonroy@gmail.com>
2023-03-10 12:15:15 -0600

    first commit

---
+++ TestDoubles/TestDoublesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDoublesApp.swift
+//  TestDoubles
+//
+//  Created by Andrea Monroy on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TestDoublesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
